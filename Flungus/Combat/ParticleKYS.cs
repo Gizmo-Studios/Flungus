1cbe3ce597fcc583899dfabc3c6d0191a9c8336f
Gizmo <Denismaximilian@web.de>
2026-01-24 16:48:49 +0100

    Add files via upload

---
+++ Flungus/Combat/ParticleKYS.cs
@@ -0,0 +1,24 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class ParticleKill : MonoBehaviour
+{
+    private ParticleSystem ps;
+
+
+    public void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    public void Update()
+    {
+        if (ps)
+        {
+            if (!ps.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
