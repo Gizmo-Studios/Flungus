210976838dafa06feb2a9b97d494a215e37a3935
Gizmo <Denismaximilian@web.de>
2026-03-08 22:18:55 +0100

    Remove unnecessary whitespace and format code

--- Flungus/Combat/SOProjectilePerk.cs
+++ Flungus/Combat/SOProjectilePerk.cs
@@ -11,15 +11,13 @@ namespace Team3.Combat
 
         [SerializeField]
         private SOVFX vFX;
-
         [SerializeField]
         private float cooldown = 0;
+        
         public SOVFX VFX => vFX;
-
         public float CoolDown => cooldown;
 
         public abstract void TriggerPerk(ProjectileCore projectile = null, ulong OwnerID = 420, Vector3 position = default(Vector3), Vector3 rotation = default(Vector3), NetworkObjectReference hitRef = default, Collision collision = null);
-
         public abstract void TriggerDeathPerk(ProjectileCore projectile = null, ulong OwnerID = 420, Vector3 position = default(Vector3), Vector3 rotation = default(Vector3), NetworkObjectReference hitRef = default, Collision collision = null);
 
         public abstract void AdjustStats(ProjectileCore projectile);
@@ -30,5 +28,6 @@ namespace Team3.Combat
         public abstract void PerkUpdate();
         public abstract void PerkApply(Vector3 position, Vector3 rotation, NetworkObjectReference hitRef = default);
     }
+
 
 }
